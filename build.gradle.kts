8a6b8815439893953c9749c7ea3bac2c4bea4c27
Karol Wisniewski <karol.wisniewski@ecom.software>
2020-10-04 10:59:43 +0200

    Application skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+plugins {
+    kotlin("jvm") version "1.4.0"
+    kotlin("kapt") version "1.4.0"
+}
+
+group = "pl.charlew"
+description="Google translate CLI"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+val developmentOnly by configurations.creating
+configurations {
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("info.picocli:picocli-codegen:4.5.0")
+    kapt("info.picocli:picocli-codegen:4.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/pl/charlew/google_translate_cli/Main.kt
@@ -0,0 +1,10 @@
+package pl.charlew.google_translate_cli
+
+import picocli.CommandLine.Command
+
+@Command(description = ["Google translate CLI command"], name = "google-translate-cli", mixinStandardHelpOptions = true)
+class Main : Runnable {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
